c945270a733c4eb1dc1a8ecd1dbc201e715d1e85
Mansoor Hussain <hussainmansoor876@gmail.com>
2019-03-03 13:00:33 +0500

    Donehshhhxsdsdssfsdx

--- Screens/Circle.js
+++ Screens/Circle.js
@@ -16,6 +16,7 @@ class Circle extends React.Component {
     this.state = {
       create: false,
       group: false,
+      openCircle: '',
       circleData: [],
       data: []
     }
@@ -54,20 +55,26 @@ class Circle extends React.Component {
     this.setState({circleData: circleData})
   }
 
-  showMap(data){
+  showMap(value){
     var { data } = this.state
     data = []
-    Object.entries(data['users']).forEach(([key,val]) => {
+    Object.entries(value['users']).forEach(([key,val1]) => {
       console.log('key',key)
-      console.log('val',val)
+      firebase.database().ref('users').child(key).on('value',(value)=>{
+        data.push(value.val())
+      })
     })
-    // console.log('data',data)
+    this.setState({
+      data: data,
+      openCircle: value['name'],
+      group: true
+    })
   }
 
 
   render() {
     const { user } = this.props
-    const { circleData, create, group, data } = this.state
+    const { circleData, create, group, data, openCircle } = this.state
     // console.log('circleData',circleData)    
     return (
         <View style={styles.container}>
@@ -80,7 +87,7 @@ class Circle extends React.Component {
         rightComponent={{style: { color: '#fff' }, icon: 'arrow-forward', color: '#fff', onPress: ()=> this.props.removeUser() }}
         />
         <ScrollView style={{marginTop: 5}}>
-        {circleData.length && !group ? circleData.map((v,i) => {
+        {circleData.length && circleData.map((v,i) => {
           return <ListItem
           key={i}
           Component={TouchableScale}
@@ -101,16 +108,44 @@ class Circle extends React.Component {
           chevron
           containerStyle={{margin: 1}}
           onPress={() => this.showMap(v)}
-        />;
-        }) : <ScrollView style={{flex: 1}}>
-        <Text>Hello</Text>
-        </ScrollView>}
-        <Button
+        />
+        })}
+        {!group ? <Button
             icon={<Icon type='font-awesome' name='users' color='#ffffff' />}
             backgroundColor='#03A9F4'
             buttonStyle={{borderRadius: 5, marginLeft: 0, marginRight: 0, marginBottom: 0, backgroundColor: 'green', marginTop: 10}}
             onPress={() => this.setState({create: true})}
-            title='CREATE CIRCLE' />
+            title='CREATE CIRCLE' /> : <ScrollView style={{flex: 1}}>
+              {data.map((v,i) => {
+                return <ListItem
+                key={i}
+                Component={TouchableScale}
+                friction={90} //
+                tension={100} // These props are passed to the parent component (here TouchableScale)
+                activeScale={0.95} //
+                linearGradientProps={{
+                  colors: ['azure', 'aqua'],
+                  start: [1, 0],
+                  end: [0.2, 0],
+                }}
+                // leftAvatar={{ rounded: true, source: { uri: avatar_url } }}
+                title={v.name}
+                titleStyle={{ color: 'white', fontWeight: 'bold' }}
+                subtitleStyle={{ color: 'white' }}
+                subtitle={v.users[user.uid] == 'Admin' ? 'You are Admin' : 'You are User'}
+                chevronColor="white"
+                chevron
+                containerStyle={{margin: 1}}
+                onPress={() => this.showMap(v)}
+              />
+              })}
+              <Button
+            icon={<Icon type='font-awesome' name='user-plus' color='#ffffff' />}
+            backgroundColor='#03A9F4'
+            buttonStyle={{borderRadius: 5, marginLeft: 0, marginRight: 0, marginBottom: 0, backgroundColor: 'green', marginTop: 10}}
+            onPress={() => this.setState({create: true})}
+            title='Invite friends' />
+            </ScrollView>}
         </ScrollView>
         </View>
          : <CreateCircle create={this.createCircle} />}
