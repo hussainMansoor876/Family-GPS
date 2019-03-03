4eb19b03cc36bba7ab1c07b7e408d16fa499617d
Mansoor Hussain <hussainmansoor876@gmail.com>
2019-03-03 10:27:39 +0500

    Donehshhhx

--- Screens/Circle.js
+++ Screens/Circle.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Alert, ScrollView } from 'react-native';
-import { Header, Button, CheckBox, Icon } from 'react-native-elements';
+import { Header, Button, CheckBox, Icon, ListItem } from 'react-native-elements';
 import { updateUser, removeUser } from '../Redux/actions/authActions'
 import { DrawerActions } from 'react-navigation-drawer';
 import { connect } from 'react-redux';
@@ -12,26 +12,38 @@ class Circle extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      create: false
+      create: false,
+      circleData: []
     }
     this.createCircle = this.createCircle.bind(this)
   }
 
   componentWillMount(){
     let { user } = this.props
+    const { circleData } = this.state
     firebase.database().ref('users').child(user.uid).on('value',(value)=>{
-      console.log('value',value.val())
       user = value.val()
+      this.props.updateUser(user)
+      user['circle'] && Object.entries(user['circle']).forEach(([key,val]) => {
+        circleData.push({name: key, users: val})
+      })
     })
   }
 
   createCircle(){
     this.setState({create: false})
+    let { user } = this.props
+    firebase.database().ref('users').child(user.uid).on('value',(value)=>{
+      user = value.val()
+      this.props.updateUser(user)
+    })
   }
 
 
   render() {
     const { user } = this.props
+    const { circleData } = this.state
+    console.log('user',user)
     const { create } = this.state
     return (
         <View style={styles.container}>
@@ -44,7 +56,14 @@ class Circle extends React.Component {
         rightComponent={{style: { color: '#fff' }, icon: 'arrow-forward', color: '#fff', onPress: ()=> this.props.removeUser() }}
         />
         <ScrollView style={{marginTop: 5}}>
-        {user['circle'] && console.log(user['circle'])}
+        {circleData.length && circleData.map((v,i) => {
+          <ListItem
+          key={i}
+          // leftAvatar={{ source: { uri: l.avatar_url } }}
+          title={v.name}
+          // subtitle={l.subtitle}
+        />
+        })}
         <Button
             icon={<Icon type='font-awesome' name='users' color='#ffffff' />}
             backgroundColor='#03A9F4'
